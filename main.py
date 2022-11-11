e900395f7875e40c7bba0cb5aa0df38d07cbde80
Cool Guy <117956187+KoalaMan12@users.noreply.github.com>
2022-11-11 19:34:35 +0000

    best out of three

--- main.py
+++ main.py
@@ -10,9 +10,8 @@
 #    If it is not, print a message to the screen and let the 
 #    Player try again
 
-
+import os
 import random
-lost = 0
 won = 0
 a=random.randint(1,3)
 print("Please type Rock, Paper or Scissors")
@@ -21,7 +20,42 @@ if a == (1):
   print("Paper")
   if str.lower(Player) == "rock":
     print("You Lose")
-  lost += 1
+  if str.lower(Player) == "scissors":
+      print("You Win")
+      won += 1
+  if str.lower(Player) == "paper":
+      print("You Tied")
+if a == (2):
+  print("Rock")
+  if str.lower(Player) == "scissors":
+    print("You Lose")
+  if str.lower(Player) == "paper":
+      print("You Win")
+      won += 1
+  if str.lower(Player) == "rock":
+      print("You Tied"
+
+if a == (3)
+  print("Scissors")
+  if str.lower(Player) == "paper":
+    print("You Lose")
+  if str.lower(Player) == "rock":
+    print("You Win")
+    won += 1
+  if str.lower(Player) == "scissors":
+      print("You Tied")
+
+
+
+import random
+a=random.randint(1,3)
+print("Please type Rock, Paper or Scissors")
+Player = input()
+os. system ('clear')
+if a == (1):
+  print("Paper")
+  if str.lower(Player) == "rock":
+    print("You Lose")
   if str.lower(Player) == "scissors":
       print("You Win")
   won += 1
@@ -33,6 +67,7 @@ if a == (2):
     print("You Lose")
   if str.lower(Player) == "paper":
       print("You Win")
+  won =+ 1
   if str.lower(Player) == "Rock":
       print("You Tied")
 if a == (3):
@@ -41,5 +76,50 @@ if a == (3):
     print("You Lose")
   if str.lower(Player) == "rock":
     print("You Win")
+  won =+ 1
   if str.lower(Player) == "scissors":
       print("You Tied")
+
+import random
+a=random.randint(1,3)
+print("Please type Rock, Paper or Scissors")
+Player = input()
+os.system ('clear')
+if a == (1):
+  print("Paper")
+  if str.lower(Player) == "rock":
+    print("You Lose")
+  if str.lower(Player) == "scissors":
+      print("You Win")
+  won += 1
+  if str.lower(Player) == "paper":
+      print("You Tied")
+
+
+if a == (2):
+  print("Rock")
+  if str.lower(Player) == "scissors":
+    print("You Lose")
+  if str.lower(Player) == "paper":
+      print("You Win")
+  won += 1
+  if str.lower(Player) == "Rock":
+      print("You Tied")
+  
+  print("You Won")
+  print(won)
+  print("out of 3")
+if a == (3):
+  print("Scissors")
+  if str.lower(Player) == "paper":
+    print("You Lose")
+  if str.lower(Player) == "rock":
+    print("You Win")
+  if str.lower(Player) == "scissors":
+      print("You Tied")
+
+  os.system ('clear')
+  print("You Won")
+  print(won)
+  print("out of")
+  print("3")
