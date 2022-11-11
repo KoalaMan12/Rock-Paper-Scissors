18f72fc8b8d3c0925a39628a0771d04562afa08c
Cool Guy <>
2022-11-11 00:42:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+a=random.randint(1,3)
+print("Please type Rock, Paper or Scissors")
+Player = input()
+if a == (1):
+  print("Paper")
+  if str.lower(Player) == "rock":
+    print("You Lose")
+  if str.lower(Player) == "scissors":
+      print("You Win")
+
+if a == (2):
+  print("Rock")
+  if str.lower(Player) == "scissors":
+    print("You Lose")
+  if str.lower(Player) == "paper":
+      print("You Win")
+
+if a == (3):
+  print("Scissors")
+  if str.lower(Player) == "paper":
+    print("You Lose")
+  if str.lower(Player) == "rock":
+    print("You Win")
